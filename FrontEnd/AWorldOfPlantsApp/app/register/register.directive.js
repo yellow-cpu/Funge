74f45065449600f2e5220533f66c8aef42ff7f2d
Dillon Heins <dillonheins@gmail.com>
2016-07-13 21:44:13 +0200

    Validation highlighting and email regex

--- FrontEnd/AWorldOfPlantsApp/app/register/register.directive.js
+++ FrontEnd/AWorldOfPlantsApp/app/register/register.directive.js
@@ -15,11 +15,27 @@ directive('switchToLogin', function($compile) {
             var password = $('#password').val();
             var confirmPassword = $('#confirmPassword').val();
 
-            if (password != confirmPassword) {
-                $('#password').css({
-                    "border": "red solid 1px"
-                });
+            var valid = true;
 
+            var errorCss = {
+                'outline': 'none',
+                'border-color': '#ff5139',
+                'box-shadow': '0 0 10px #ff5139'
+            };
+
+            if (password != confirmPassword) {
+                $('#confirmPassword').css(errorCss);
+                valid = false;
+            }
+
+            var emailPattern = /\A[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\z"/i;
+
+            if (email.match(emailPattern)) {
+                $('#email').css(errorCss);
+                valid = false;
+            }
+
+            if (!valid) {
                 return;
             }
 
