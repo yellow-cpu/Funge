f64116f488b5533c14040529fcdf12d35c6d7811
Dillon Heins <dillonheins@gmail.com>
2016-07-23 21:45:52 +0200

    Base for displaying plants added

--- FrontEnd/AWorldOfPlantsApp/app/plants/plants.component.js
+++ FrontEnd/AWorldOfPlantsApp/app/plants/plants.component.js
@@ -2,8 +2,27 @@
 
 // Register `plants` component, along with its associated controller and template
 angular.module('plants').component('plants', {
-    templateUrl: 'plants/plants.template.html',
-    controller: function PlantsController() {
-        var self = this;
-    }
+  templateUrl: 'plants/plants.template.html',
+  controller: function PlantsController() {
+    var self = this;
+
+    var apigClient = apigClientFactory.newClient(AWS.config.credentials);
+
+    var params = {};
+
+    var body = {};
+
+    apigClient.plantsGet(params, body)
+      .then(function (result) {
+        console.log("Success: " + JSON.stringify(result.data));
+      }).catch(function (result) {
+      console.log("Error: " + JSON.stringify(result));
+    });
+
+    self.plants = [{
+      'name': 'bob',
+      'type': 'flower',
+      'id': 'asasdasdsa'
+    }];
+  }
 });
