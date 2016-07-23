cfd1e94518ffb9e7649187517e78f447f67d955e
Dillon Heins <dillonheins@gmail.com>
2016-07-23 19:49:59 +0200

    Dashboard - WIP

--- FrontEnd/AWorldOfPlantsApp/app/site/site.directive.js
+++ FrontEnd/AWorldOfPlantsApp/app/site/site.directive.js
@@ -3,9 +3,7 @@
 // Register `site` directive
 angular.module('site').directive('site', function ($compile) {
   var linkFunction = function(scope, element, attributes) {
-    $(document).ready(function() {
-      //$('#side-menu').metisMenu();
-    });
+  
   };
 
   return {
