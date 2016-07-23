5d3b99539efc4f609f86b1b5923dc20426bd4fc7
Dillon Heins <dillonheins@gmail.com>
2016-07-23 17:22:36 +0200

    Fixed scrolling issue

--- FrontEnd/AWorldOfPlantsApp/app/core/master/master.directive.js
+++ FrontEnd/AWorldOfPlantsApp/app/core/master/master.directive.js
@@ -5,17 +5,7 @@ angular.
   module('master').
   directive('wrapper', function() {
     var linkFunction = function(scope, element, attributes) {
-      var wrapper = $("#wrapper");
-
-      var resizeFunction = function() {
-        $(wrapper).css({
-          'width': $(window).width(),
-          'height': $(window).height()
-        });
-      };
-
-      $(document).ready(resizeFunction);
-      $(window).resize(resizeFunction);
+      
     };
 
     return {
