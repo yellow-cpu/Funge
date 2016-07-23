7d7b6704ced1d3b2a07ba169c7e0f8363b76d07c
Gian Paolo Buffo <gpbuffo@gmail.com>
2016-07-23 14:58:05 +0200

    Temporary animations added

--- FrontEnd/AWorldOfPlantsApp/app/login/login.directive.js
+++ FrontEnd/AWorldOfPlantsApp/app/login/login.directive.js
@@ -6,7 +6,14 @@ module('login').
 directive('switchToRegister', function($compile) {
     var linkFunction = function(scope, element, attributes) {
         $('#switchToRegister').on('click', function() {
-            $('#my-tab-content').html($compile('<register></register>')(scope));
+            $('login').animate({
+               height: 'toggle',
+                opacity: 0
+            }, 500, function() {
+                $('#my-tab-content').html($compile('<register></register>')(scope));
+
+            });
+
         });
     };
 
@@ -14,4 +21,3 @@ directive('switchToRegister', function($compile) {
         link: linkFunction
     };
 });
-
