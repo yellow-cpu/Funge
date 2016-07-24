8d87081eae0def8b4c1bc7ccbd115ffaa164f6a0
Dillon Heins <dillonheins@gmail.com>
2016-07-24 11:15:14 +0200

    Plants get and displaying working

--- FrontEnd/AWorldOfPlantsApp/app/plants/plants.component.js
+++ FrontEnd/AWorldOfPlantsApp/app/plants/plants.component.js
@@ -3,8 +3,10 @@
 // Register `plants` component, along with its associated controller and template
 angular.module('plants').component('plants', {
   templateUrl: 'plants/plants.template.html',
-  controller: function PlantsController() {
+  controller: function PlantsController($scope) {
     var self = this;
+
+    self.plants = [];
 
     var apigClient = apigClientFactory.newClient(AWS.config.credentials);
 
@@ -15,14 +17,10 @@ angular.module('plants').component('plants', {
     apigClient.plantsGet(params, body)
       .then(function (result) {
         console.log("Success: " + JSON.stringify(result.data));
+        self.plants = result.data.plants;
+        $scope.$apply();
       }).catch(function (result) {
-      console.log("Error: " + JSON.stringify(result));
+        console.log("Error: " + JSON.stringify(result));
     });
-
-    self.plants = [{
-      'name': 'bob',
-      'type': 'flower',
-      'id': 'asasdasdsa'
-    }];
   }
 });
