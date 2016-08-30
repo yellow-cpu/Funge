d5737cd112c1f373466520fba5dc09bf61acb2b7
Dillon Heins <dillonheins@gmail.com>
2016-08-30 12:03:00 +0200

    Frontend path updated travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,5 +5,5 @@ echo "--- CUSTOM DEPLOYMENT SCRIPT RUNNING ---"
 aws s3 cp a-world-of-plants-1.6.3-SNAPSHOT.jar s3://a-world-of-plants-user-management
 aws lambda update-function-code --function-name user-management --s3-bucket a-world-of-plants-user-management --s3-key a-world-of-plants-1.6.3-SNAPSHOT.jar
 
-cd ../../../FrontEnd/AWorldOfPlantsApp/app/
+cd ../../../FrontEnd/AWorldOfPlantsApp/
 aws s3 cp app s3://a-world-of-plants-frontend --recursive --exclude "*.log"
