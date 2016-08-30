f33477377acddf60944c64c92da1613477f3ec17
Dillon Heins <dillonheins@gmail.com>
2016-08-30 10:35:25 +0200

    Travis custom bash script test

---
+++ scripts/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "--- CUSTOM DEPLOYMENT SCRIPT RUNNING ---"
+ls
+aws s3 cp a-world-of-plants-1.6.3-SNAPSHOT.jar s3://a-world-of-plants-user-management
+aws lambda update-function-code --function-name user-management --s3-bucket a-world-of-plants-user-management --s3-key a-world-of-plants-1.6.3-SNAPSHOT.jar
