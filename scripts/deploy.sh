44723fb1010b07a9a29dac0c4a53749145a43293
Dillon Heins <dillonheins@gmail.com>
2016-08-30 11:23:47 +0200

    Custom deployment script with new environment variables

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 echo "--- CUSTOM DEPLOYMENT SCRIPT RUNNING ---"
-ssh
-# aws s3 cp a-world-of-plants-1.6.3-SNAPSHOT.jar s3://a-world-of-plants-user-management
-# aws lambda update-function-code --function-name user-management --s3-bucket a-world-of-plants-user-management --s3-key a-world-of-plants-1.6.3-SNAPSHOT.jar
+aws s3 cp a-world-of-plants-1.6.3-SNAPSHOT.jar s3://a-world-of-plants-user-management
+aws lambda update-function-code --function-name user-management --s3-bucket a-world-of-plants-user-management --s3-key a-world-of-plants-1.6.3-SNAPSHOT.jar
