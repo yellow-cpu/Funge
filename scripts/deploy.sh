f4f75164005d7ab90c7db4ba1b339e91f10291d0
Dillon Heins <dillonheins@gmail.com>
2016-09-04 11:13:16 +0200

    npm install added to travis deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,4 +6,5 @@ aws s3 cp a-world-of-plants-1.6.3-SNAPSHOT.jar s3://a-world-of-plants-user-manag
 aws lambda update-function-code --function-name user-management --s3-bucket a-world-of-plants-user-management --s3-key a-world-of-plants-1.6.3-SNAPSHOT.jar
 
 cd ../../../FrontEnd/AWorldOfPlantsApp/
+npm install
 aws s3 cp app s3://funge.cf --recursive --exclude "*.log"
