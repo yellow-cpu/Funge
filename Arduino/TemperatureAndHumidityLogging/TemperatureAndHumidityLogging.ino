6d7f47caaf6c6d5b50bce7949a74422e32655256
Matthias Harvey <matthiasharvey@gmail.com>
2016-09-05 22:55:17 +0200

    Add light control to sketch

--- Arduino/TemperatureAndHumidityLogging/TemperatureAndHumidityLogging.ino
+++ Arduino/TemperatureAndHumidityLogging/TemperatureAndHumidityLogging.ino
@@ -6,6 +6,9 @@
 
 
 #define DHTPIN A0     // what pin we're connected to
+#define RED_PIN 3
+#define GREEN_PIN 5
+#define BLUE_PIN 6
 
 // Uncomment whatever type you're using!
 //#define DHTTYPE DHT11   // DHT 11 
@@ -21,12 +24,16 @@ DHT dht(DHTPIN, DHTTYPE);
 
 aws_iot_mqtt_client myClient;
 char JSON_buf[100];
-char JSON_buf2[100];
+char JSON_buf_red[10];
+char JSON_buf_green[10];
+char JSON_buf_blue[10];
 char float_buf[5];
 char float_buf2[5];
 
 float desiredTemp = 70.0;
 float test = 12.0;
+
+int red = 0, green = 0, blue = 0;
 
 bool success_connect = false;
 
@@ -52,8 +59,14 @@ void msg_callback_delta(char* src, unsigned int len, Message_status_t flag) {
   if(flag == STATUS_NORMAL) {
     // Get the whole delta section
     print_log("getDeltaKeyValue", myClient.getDeltaValueByKey(src, "", JSON_buf, 100));
-    print_log("getDeltaKeyValueTest", myClient.getDeltaValueByKey(src, "test", JSON_buf2, 100));
-    test = atof(JSON_buf2);
+    print_log("getDeltaKeyValueRed", myClient.getDeltaValueByKey(src, "red", JSON_buf_red, 100));
+    print_log("getDeltaKeyValueGreen", myClient.getDeltaValueByKey(src, "green", JSON_buf_green, 100));
+    print_log("getDeltaKeyValueBlue", myClient.getDeltaValueByKey(src, "blue", JSON_buf_blue, 100));
+    
+    red = atoi(JSON_buf_red);
+    green = atoi(JSON_buf_green);
+    blue = atoi(JSON_buf_blue);
+    
     String payload = "{\"state\":{\"reported\":";
     payload += JSON_buf;
     payload += "}}";
@@ -64,8 +77,12 @@ void msg_callback_delta(char* src, unsigned int len, Message_status_t flag) {
 
 void setup() {
   Serial.begin(115200);
- // delay(1000);
-  while(!Serial);
+  delay(1000);
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(BLUE_PIN, OUTPUT);
+  
+  //while(!Serial);
 
   char curr_version[80];
   sprintf(curr_version, "AWS IoT SDK Version(dev) %d.%d.%d-%s\n", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, VERSION_TAG);
@@ -83,6 +100,10 @@ void setup() {
 }
 
 void loop() {
+  analogWrite(RED_PIN, red);
+  analogWrite(GREEN_PIN, green);
+  analogWrite(BLUE_PIN, blue);
+
   if(success_connect) {
     // Reading temperature or humidity takes about 250 milliseconds!
     // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
