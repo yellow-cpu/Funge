3f33e5ae7da0a823c3c4be65ea3021149e1862dd
Gian Paolo Buffo <gpbuffo@gmail.com>
2016-09-06 16:49:42 +0200

    Timeline backend logic

--- BackEnd/a-world-of-plants/src/main/java/cf/funge/aworldofplants/action/CreatePlantAction.java
+++ BackEnd/a-world-of-plants/src/main/java/cf/funge/aworldofplants/action/CreatePlantAction.java
@@ -21,6 +21,8 @@ import cf.funge.aworldofplants.model.action.CreatePlantRequest;
 import cf.funge.aworldofplants.model.action.CreatePlantResponse;
 import cf.funge.aworldofplants.model.plant.Plant;
 import cf.funge.aworldofplants.model.plant.PlantDAO;
+import cf.funge.aworldofplants.model.timeline.TimelineEvent;
+import cf.funge.aworldofplants.model.timeline.TimelineDAO;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.google.gson.JsonObject;
@@ -45,6 +47,7 @@ public class CreatePlantAction extends AbstractAction {
         }
 
         PlantDAO dao = DAOFactory.getPlantDAO();
+        TimelineDAO timelineDAO = DAOFactory.getTimelineDAO();
 
         Plant newPlant = new Plant();
         newPlant.setUsername(input.getUsername());
@@ -53,10 +56,18 @@ public class CreatePlantAction extends AbstractAction {
         newPlant.setPlantAge(input.getPlantAge());
         newPlant.setColour(input.getColour());
 
+        TimelineEvent timelineEvent = new TimelineEvent();
+        timelineEvent.setUsername(input.getUsername());
+        timelineEvent.setMessage("Created plant with name " + input.getPlantName());
+        timelineEvent.setCategory("Good");
+        timelineEvent.setTimestamp(5);
+        timelineEvent.setPointValue(420);
+
         //ToDo: make plant names unique for users
         //ToDo: assign colour to each plant
 
         String plantId;
+        String timelineEventId;
 
         try {
             plantId = dao.createPlant(newPlant);
@@ -67,6 +78,18 @@ public class CreatePlantAction extends AbstractAction {
 
         if (plantId == null || plantId.trim().equals("")) {
             logger.log("PlantID is null or empty");
+            throw new InternalErrorException(ExceptionMessages.EX_DAO_ERROR);
+        }
+
+        try {
+            timelineEventId = timelineDAO.createTimelineEvent(timelineEvent);
+        } catch (final DAOException e) {
+            logger.log("Error while creating new timeline event\n" + e.getMessage());
+            throw new InternalErrorException(ExceptionMessages.EX_DAO_ERROR);
+        }
+
+        if (timelineEventId == null || timelineEventId.trim().equals("")) {
+            logger.log("TimelineEventId is null or empty");
             throw new InternalErrorException(ExceptionMessages.EX_DAO_ERROR);
         }
 
---
+++ BackEnd/a-world-of-plants/src/main/java/cf/funge/aworldofplants/model/timeline/DDBTimelineDAO.java
@@ -0,0 +1,112 @@
+package cf.funge.aworldofplants.model.timeline;
+
+import cf.funge.aworldofplants.configuration.DynamoDBConfiguration;
+import cf.funge.aworldofplants.exception.DAOException;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DDBTimelineDAO implements TimelineDAO {
+    private static DDBTimelineDAO instance = null;
+
+    // Credentials for the client come from the environment variables pre-configured by Lambda. These are tied to the
+    // Lambda function execution role.
+    private static AmazonDynamoDBClient ddbClient = new AmazonDynamoDBClient();
+
+    protected DDBTimelineDAO() {
+        // constructor is protected so that it can't be called from the outside
+    }
+
+    /**
+     * Returns the initialized default instance of the TimelineDAO
+     *
+     * @return An initialized TimelineDAO instance
+     */
+    public static DDBTimelineDAO getInstance() {
+        if (instance == null) {
+            instance = new DDBTimelineDAO();
+        }
+
+        return instance;
+    }
+
+    /**
+     * Creates a new Timeline Event
+     *
+     * @param timelineEvent The TimelineEvent object to be created
+     * @return The id for the newly created TimelineEvent object
+     * @throws DAOException
+     */
+    public String createTimelineEvent(TimelineEvent timelineEvent) throws DAOException {
+        if (timelineEvent.getMessage() == null || timelineEvent.getMessage().trim().equals("")) {
+            throw new DAOException("Cannot lookup null or empty timelineEvent");
+        }
+
+        getMapper().save(timelineEvent);
+
+        return timelineEvent.getTimelineEventId();
+    }
+
+    /**
+     * Gets a Plant by its id
+     *
+     * @param timelineEventId The timelineEventId to look for
+     * @return An initialized TimelineEvent object, null if the TimelineEvent could not be found
+     * @throws DAOException
+     */
+    public TimelineEvent getTimelineEventbyId(String timelineEventId) throws DAOException {
+        if (timelineEventId == null || timelineEventId.trim().equals("")) {
+            throw new DAOException("Cannot lookup null or empty plantId");
+        }
+
+        return getMapper().load(TimelineEvent.class, timelineEventId);
+    }
+
+    /**
+     * Returns a list of timelineEvents for a user in the DynamoDB table.
+     *
+     * @param limit The maximum numbers of results for the scan
+     * @return A List of TimelineEvent objects
+     */
+    public List<TimelineEvent> getUserTimelineEvents(int limit, String username) {
+        if (limit <= 0 || limit > DynamoDBConfiguration.SCAN_LIMIT)
+            limit = DynamoDBConfiguration.SCAN_LIMIT;
+
+        Map<String, AttributeValue> eav = new HashMap<>();
+        eav.put(":val1", new AttributeValue().withS(username));
+
+        DynamoDBScanExpression expression = new DynamoDBScanExpression()
+                .withFilterExpression("username = :val1")
+                .withExpressionAttributeValues(eav);
+        expression.setLimit(limit);
+
+        List<TimelineEvent> scanResult = getMapper().scan(TimelineEvent.class, expression);
+
+        return scanResult;
+    }
+
+    public String deleteTimelineEvent(String timelineEventId) {
+        try {
+            getMapper().delete(getTimelineEventbyId(timelineEventId));
+        } catch (DAOException e) {
+
+        }
+
+        return "Timeline event deleted successfully";
+    }
+
+    /**
+     * Returns a DynamoDBMapper object initialized with the default DynamoDB client
+     *
+     * @return An initialized DynamoDBMapper
+     */
+    protected DynamoDBMapper getMapper() {
+        return new DynamoDBMapper(ddbClient, new DynamoDBMapperConfig(DynamoDBMapperConfig.SaveBehavior.UPDATE));
+    }
+}
