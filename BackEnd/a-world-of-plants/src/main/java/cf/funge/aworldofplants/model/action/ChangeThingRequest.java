f3a59683a9dd03b2f1fd18c5fae7c054dfc0ed0f
Matthias Harvey <matthiasharvey@gmail.com>
2016-10-03 14:52:59 +0200

    Remove unnecessary properties for update thing request

--- BackEnd/a-world-of-plants/src/main/java/cf/funge/aworldofplants/model/action/ChangeThingRequest.java
+++ BackEnd/a-world-of-plants/src/main/java/cf/funge/aworldofplants/model/action/ChangeThingRequest.java
@@ -1,85 +1,12 @@
 package cf.funge.aworldofplants.model.action;
 
-import java.util.List;
-
 /**
- * Bean for the update thing by id request.
+ * Bean for the update thing by thing name request.
  */
 public class ChangeThingRequest {
-    private String thingId;
-    private String thingName;
-    private String username;
     private String plantId;
-    private String policyName;
-    private String certificateArn;
-    private String certificateId;
-    private String mqttTopic;
-    private List<String> files;
     private String colour;
-
-    public String getPolicyName() {
-        return policyName;
-    }
-
-    public void setPolicyName(String policyName) {
-        this.policyName = policyName;
-    }
-
-    public String getCertificateArn() {
-        return certificateArn;
-    }
-
-    public void setCertificateArn(String certificateArn) {
-        this.certificateArn = certificateArn;
-    }
-
-    public String getCertificateId() {
-        return certificateId;
-    }
-
-    public void setCertificateId(String certificateId) {
-        this.certificateId = certificateId;
-    }
-
-    public String getMqttTopic() {
-        return mqttTopic;
-    }
-
-    public void setMqttTopic(String mqttTopic) {
-        this.mqttTopic = mqttTopic;
-    }
-
-    public List<String> getFiles() {
-        return files;
-    }
-
-    public void setFiles(List<String> files) {
-        this.files = files;
-    }
-
-    public String getThingId() {
-        return thingId;
-    }
-
-    public void setThingId(String thingId) {
-        this.thingId = thingId;
-    }
-
-    public String getThingName() {
-        return thingName;
-    }
-
-    public void setThingName(String thingName) {
-        this.thingName = thingName;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
+    private String thingName;
 
     public String getPlantId() {
         return plantId;
@@ -95,5 +22,13 @@ public class ChangeThingRequest {
 
     public void setColour(String colour) {
         this.colour = colour;
+    }
+
+    public String getThingName() {
+        return thingName;
+    }
+
+    public void setThingName(String thingName) {
+        this.thingName = thingName;
     }
 }
