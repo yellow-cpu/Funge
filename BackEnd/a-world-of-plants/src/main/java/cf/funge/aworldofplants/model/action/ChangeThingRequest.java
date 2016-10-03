3c870b732f6ef257956775853c9a8340f83a1f13
Matthias Harvey <matthiasharvey@gmail.com>
2016-10-03 15:02:09 +0200

    Minor change to change thing request structure

--- BackEnd/a-world-of-plants/src/main/java/cf/funge/aworldofplants/model/action/ChangeThingRequest.java
+++ BackEnd/a-world-of-plants/src/main/java/cf/funge/aworldofplants/model/action/ChangeThingRequest.java
@@ -4,9 +4,26 @@ package cf.funge.aworldofplants.model.action;
  * Bean for the update thing by thing name request.
  */
 public class ChangeThingRequest {
+    private String thingName;
+    private String username;
     private String plantId;
     private String colour;
-    private String thingName;
+
+    public String getThingName() {
+        return thingName;
+    }
+
+    public void setThingName(String thingName) {
+        this.thingName = thingName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
     public String getPlantId() {
         return plantId;
@@ -22,13 +39,5 @@ public class ChangeThingRequest {
 
     public void setColour(String colour) {
         this.colour = colour;
-    }
-
-    public String getThingName() {
-        return thingName;
-    }
-
-    public void setThingName(String thingName) {
-        this.thingName = thingName;
     }
 }
