cbe6051bdee63f118c403bd841e9c38099d578b1
Dillon Heins <dillonheins@gmail.com>
2016-10-09 14:21:06 +0200

    Update button feedback for plants completed

--- FrontEnd/AWorldOfPlantsApp/app/components/plant-detail/plant-detail.component.js
+++ FrontEnd/AWorldOfPlantsApp/app/components/plant-detail/plant-detail.component.js
@@ -193,9 +193,36 @@ angular.module('plantDetail').component('plantDetail', {
     self.updatePlant = function() {
       var params = {};
       var body = self.plantDetails;
+      var plantUpdate = $('#plantUpdate');
+
+      plantUpdate.find('span').css({
+        "display": "none"
+      });
+
+      plantUpdate.find('.update-spin').css({
+        "display": "inline-block"
+      });
 
       apigClient.plantsUpdatePost(params, body)
         .then(function (result) {
+          plantUpdate.find('.update-spin').css({
+            "display": "none"
+          });
+
+          plantUpdate.find('svg').css({
+            "display": "block"
+          });
+
+          setTimeout(function () {
+            plantUpdate.find('span').css({
+              "display": "inline"
+            });
+
+            plantUpdate.find('svg').css({
+              "display": "none"
+            });
+          }, 3000);
+
           console.log("Success: " + JSON.stringify(result));
         }).catch(function (result) {
           console.log("Error: " + JSON.stringify(result));
