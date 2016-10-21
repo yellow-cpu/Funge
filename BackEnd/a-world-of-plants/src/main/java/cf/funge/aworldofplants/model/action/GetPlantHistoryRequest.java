297ef1cd52b9926a629fd06fc8d6164d584927ca
Dillon Heins <dillonheins@gmail.com>
2016-10-21 17:52:28 +0200

    Get plant history - WIP

--- BackEnd/a-world-of-plants/src/main/java/cf/funge/aworldofplants/model/action/GetPlantHistoryRequest.java
+++ BackEnd/a-world-of-plants/src/main/java/cf/funge/aworldofplants/model/action/GetPlantHistoryRequest.java
@@ -5,4 +5,39 @@ package cf.funge.aworldofplants.model.action;
  */
 public class GetPlantHistoryRequest {
     String plantId;
+    String chartType;
+    String startDate;
+    String endDate;
+
+    public String getPlantId() {
+        return plantId;
+    }
+
+    public void setPlantId(String plantId) {
+        this.plantId = plantId;
+    }
+
+    public String getChartType() {
+        return chartType;
+    }
+
+    public void setChartType(String chartType) {
+        this.chartType = chartType;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
 }
