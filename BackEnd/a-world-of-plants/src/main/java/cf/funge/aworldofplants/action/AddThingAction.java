c244f600f08f1d4335a6a066fd920dc9122f1097
Dillon Heins <dillonheins@gmail.com>
2016-10-24 02:21:41 +0200

    Thing validation

--- BackEnd/a-world-of-plants/src/main/java/cf/funge/aworldofplants/action/AddThingAction.java
+++ BackEnd/a-world-of-plants/src/main/java/cf/funge/aworldofplants/action/AddThingAction.java
@@ -66,7 +66,7 @@ public class AddThingAction extends AbstractAction {
         ThingDAO thingDAO = DAOFactory.getThingDAO();
 
         try {
-            if (thingDAO.getThingByName(input.getThingName()) != null) {
+            if (thingDAO.getThingByName(input.getThingName()).getThingName() == input.getThingName()) {
                 throw new BadRequestException("thing already exists");
             }
         } catch (final DAOException e) {
